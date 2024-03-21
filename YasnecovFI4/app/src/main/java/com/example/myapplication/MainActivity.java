d27ddb6bbe452a1e03871ca8c7ff4c4543fdd5a2
Leturgone <finekon221@gmail.com>
2024-03-22 02:01:10 +0300

    передача данных из первой во вторую

--- YasnecovFI4/app/src/main/java/com/example/myapplication/MainActivity.java
+++ YasnecovFI4/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        Button NextButton = findViewById(R.id.next_button);
+        NextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Получение ссылок на виджеты
+                EditText nameText = findViewById(R.id.text_name);
+                EditText surText = findViewById(R.id.text_surname);
+
+                //Получение значений
+                String name = nameText.getText().toString();
+                String surname = surText.getText().toString();
+
+                //Отправка значений в SecondActivity
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("name",name);
+                intent.putExtra("surname",surname);
+                startActivity(intent);
+            }
+        });
 
     }
 }
