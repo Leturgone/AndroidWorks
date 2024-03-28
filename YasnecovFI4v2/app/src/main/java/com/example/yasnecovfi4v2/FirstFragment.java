8baf00f4f5de8b9575c09984483c0f528069cb30
Leturgone <finekon221@gmail.com>
2024-03-28 20:58:56 +0300

    сделано

--- YasnecovFI4v2/app/src/main/java/com/example/yasnecovfi4v2/FirstFragment.java
+++ YasnecovFI4v2/app/src/main/java/com/example/yasnecovfi4v2/FirstFragment.java
@@ -24,15 +24,6 @@ public class FirstFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FirstFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static FirstFragment newInstance(String param1, String param2) {
         FirstFragment fragment = new FirstFragment();
         Bundle args = new Bundle();
