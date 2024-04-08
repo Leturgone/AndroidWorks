2bea4cce64046f7ac9419ba1bd460b110a9836e9
Leturgone <finekon221@gmail.com>
2024-04-09 01:18:58 +0300

    Сделать реалтзацю нажатий с фрагментами + экшен бар со стрелкой

--- YasnecovFI6/app/src/main/java/com/example/yasnecovfi6/MainActivity.java
+++ YasnecovFI6/app/src/main/java/com/example/yasnecovfi6/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
     public ActionBarDrawerToggle toggle;
@@ -29,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         drawer = findViewById(R.id.drawer_layout);
         toggle = new ActionBarDrawerToggle(
                 MainActivity.this, drawer, R.string.drawler_open, R.string.drawler_close);
+
         if (drawer != null){
             drawer.addDrawerListener(toggle);
         }
@@ -36,36 +38,39 @@ public class MainActivity extends AppCompatActivity {
         //синхронизирует текущее состояние
         // drawerLayout (открыт или закрыт) с позицией связанной
         // с ним кнопки на панели действий (ActionBar).
-
         toggle.syncState();
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
         actionBar.setTitle("Постеры фильма");
-//        BottomNavigationView bottomNavigationView = findViewById(R.id.drawer_layout);
-//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
-//
-//            @Override
-//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//                if (item.getItemId() == R.id.poster1){
-//                    //Обработка выбора раздела Постер 1
-//                    Toast.makeText(MainActivity.this,"Постер 1",Toast.LENGTH_LONG).show();
-//                }
-//                else if (item.getItemId() == R.id.poster2){
-//                    //Обработка выбора раздела Постер 1
-//                    Toast.makeText(MainActivity.this,"Постер 2",Toast.LENGTH_LONG).show();
-//                }
-//                else if (item.getItemId() == R.id.poster3){
-//                    //Обработка выбора раздела Постер 1
-//                    Toast.makeText(MainActivity.this,"Постер 3",Toast.LENGTH_LONG).show();
-//                }
-//                else if (item.getItemId() == R.id.on_next_avtivity){
-//                    //Обработка выбора раздела Постер 1
-//                    Toast.makeText(MainActivity.this,"on next activity",Toast.LENGTH_LONG).show();
-//                }
-//                return false;
-//            }
-//        });
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.poster1){
+                    //Обработка выбора раздела Постер 1
+                    Toast.makeText(MainActivity.this,"Постер 1",Toast.LENGTH_LONG).show();
+                }
+                else if (item.getItemId() == R.id.poster2){
+                    //Обработка выбора раздела Постер 1
+                    Toast.makeText(MainActivity.this,"Постер 2",Toast.LENGTH_LONG).show();
+                }
+                else if (item.getItemId() == R.id.poster3){
+                    //Обработка выбора раздела Постер 1
+                    Toast.makeText(MainActivity.this,"Постер 3",Toast.LENGTH_LONG).show();
+                }
+                else if (item.getItemId() == R.id.on_next_avtivity){
+                    //Обработка выбора раздела Постер 1
+                    Toast.makeText(MainActivity.this,"on next activity",Toast.LENGTH_LONG).show();
+                }
+                return false;
+            }
+        });
+
+
     }
 
     // Обработка нажатия на иконку меню в ActionBar для открытия и закрытия Drawer
