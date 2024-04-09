d6f7317ad44dd68e71beadaed41fd2d53f68e7e7
Leturgone <finekon221@gmail.com>
2024-04-09 12:04:00 +0300

    Сделан переход между фрагментами, осталось сделать отображение выбора на панели

--- YasnecovFI6/app/src/main/java/com/example/yasnecovfi6/MainActivity.java
+++ YasnecovFI6/app/src/main/java/com/example/yasnecovfi6/MainActivity.java
@@ -6,7 +6,9 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -55,21 +57,30 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment tempFragment = null;
                 if (item.getItemId() == R.id.poster1){
                     //Обработка выбора раздела Постер 1
+                    tempFragment = new Poster1Fragment();
                     Toast.makeText(MainActivity.this,"Постер 1",Toast.LENGTH_LONG).show();
                 }
                 else if (item.getItemId() == R.id.poster2){
                     //Обработка выбора раздела Постер 1
+                    tempFragment = new Poster2Fragment();
                     Toast.makeText(MainActivity.this,"Постер 2",Toast.LENGTH_LONG).show();
                 }
                 else if (item.getItemId() == R.id.poster3){
                     //Обработка выбора раздела Постер 1
+                    tempFragment = new Poster3Fragment();
                     Toast.makeText(MainActivity.this,"Постер 3",Toast.LENGTH_LONG).show();
                 }
                 else if (item.getItemId() == R.id.on_next_avtivity){
                     //Обработка выбора раздела Постер 1
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                     Toast.makeText(MainActivity.this,"on next activity",Toast.LENGTH_LONG).show();
+                    startActivity(intent);
+                }
+                if (tempFragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment, tempFragment).commit();
                 }
                 return false;
             }
