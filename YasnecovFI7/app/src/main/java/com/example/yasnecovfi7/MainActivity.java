c70dc53d3de6af3f4b8a5bdf3759939a4312021d
Leturgone <finekon221@gmail.com>
2024-04-10 02:29:31 +0300

    Сделано первое задание

--- YasnecovFI7/app/src/main/java/com/example/yasnecovfi7/MainActivity.java
+++ YasnecovFI7/app/src/main/java/com/example/yasnecovfi7/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.yasnecovfi7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Запуск сервиса для вопроизведения музыки
+        Intent startIntant = new Intent(this, MyService.class);
+        startService(startIntant);
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        //Остановка сервиса музыки после уничтоения активности
+        Intent stopIntant = new Intent(this, MyService.class);
+        stopService(stopIntant);
     }
 }
