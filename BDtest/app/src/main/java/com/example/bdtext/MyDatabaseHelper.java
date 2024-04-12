91b638b2cb0297842fc1c75398176b9705f8f892
Leturgone <finekon221@gmail.com>
2024-04-13 00:49:24 +0300

    Создан класс DataBaseHelper

---
+++ BDtest/app/src/main/java/com/example/bdtext/MyDatabaseHelper.java
@@ -0,0 +1,41 @@
+package com.example.bdtext;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    private Context context;
+    private static final String DATABASE_NAME = "BookLibary.db"; //название бд
+    private static final int DATABASE_VERSION = 1; //версия бд
+    private static final String TABLE_NAME = "my_libary"; // название таблицы в бд
+    // названия столбцов
+    private static final String COLUMN_ID ="_id";
+    private static final String COLUMN_TITLE ="book_title";
+    private static final String COLUMN_AUTHOR ="book_author";
+    private static final String COLUMN_PAGES ="book_pages";
+
+    public MyDatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + TABLE_NAME +
+                        " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        COLUMN_TITLE + " TEXT, "+
+                        COLUMN_AUTHOR + " TEXT, " +
+                        COLUMN_PAGES + " INTEGER);";
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME); //удаляет таблицу
+        onCreate(db);
+    }
+}
