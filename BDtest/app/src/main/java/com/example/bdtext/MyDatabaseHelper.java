6b66366e19d798707cd2d5bf2996a44c275a4a92
Leturgone <finekon221@gmail.com>
2024-04-15 23:45:19 +0300

    Создана функция для добавления элементов

--- BDtest/app/src/main/java/com/example/bdtext/MyDatabaseHelper.java
+++ BDtest/app/src/main/java/com/example/bdtext/MyDatabaseHelper.java
@@ -1,8 +1,10 @@
 package com.example.bdtext;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -37,5 +39,22 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME); //удаляет таблицу
         onCreate(db);
+    }
+
+    void addBook(String title, String author, int pages){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_TITLE,title);
+        cv.put(COLUMN_AUTHOR,author);
+        cv.put(COLUMN_PAGES,pages);
+
+        long result = db.insert(TABLE_NAME,null,cv);
+        if(result == -1){
+            Toast.makeText(context,"Ошибка", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(context,"Успешно добавлено", Toast.LENGTH_SHORT).show();
+        }
     }
 }
