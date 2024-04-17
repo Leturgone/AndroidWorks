d5d52d345f8e756d93424d7ec6b1efb3cd97788a
Leturgone <145966860+Leturgone@users.noreply.github.com>
2024-04-17 15:13:31 +0300

    Сделано

--- YasnecovFI6/app/src/main/java/com/example/yasnecovfi6/SecondActivity.java
+++ YasnecovFI6/app/src/main/java/com/example/yasnecovfi6/SecondActivity.java
@@ -11,6 +11,7 @@ import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+
 public class SecondActivity extends AppCompatActivity {
     public Toolbar toolbar;
     @Override
