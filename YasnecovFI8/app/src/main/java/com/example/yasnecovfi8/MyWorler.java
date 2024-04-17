9f2143c7d8836d2903ab0cd31e9d9a9f50931a7e
Leturgone <finekon221@gmail.com>
2024-04-17 15:43:06 +0300

    Создан класс воркера

---
+++ YasnecovFI8/app/src/main/java/com/example/yasnecovfi8/MyWorler.java
@@ -0,0 +1,28 @@
+package com.example.yasnecovfi8;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class MyWorler extends Worker {
+    public final String TAG = "MY_TAG";
+    public MyWorler(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        Log.v(TAG, "Work is in progress");
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Log.v(TAG, "Work finished");
+        return Worker.Result.success();
+    }
+}
