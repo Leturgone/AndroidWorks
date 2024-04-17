8a3f643d8dced3d38050f7b97cf58672b8aa7f4d
Leturgone <145966860+Leturgone@users.noreply.github.com>
2024-04-17 15:12:20 +0300

    Сделано

--- YasnecovFI4v2/app/src/main/java/com/example/yasnecovfi4v2/FirstFragment.java
+++ YasnecovFI4v2/app/src/main/java/com/example/yasnecovfi4v2/FirstFragment.java
@@ -11,8 +11,6 @@ import android.view.ViewGroup;
 
 public class FirstFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
