c237973c737cb1800ecfcdd5dd1b9dc122d710cb
Leturgone <finekon221@gmail.com>
2024-04-18 13:27:22 +0300

    Сделано

--- YasnecovFI8/app/src/main/java/com/example/yasnecovfi8/MainActivity.java
+++ YasnecovFI8/app/src/main/java/com/example/yasnecovfi8/MainActivity.java
@@ -6,20 +6,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkManager;
 
-import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.squareup.picasso.Picasso;
