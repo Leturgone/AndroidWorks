86424be3fedd8cb0c617478e66909bf348b0cbe7
Leturgone <finekon221@gmail.com>
2024-04-18 13:24:43 +0300

    Сделано

--- YasnecovFI8/app/build.gradle.kts
+++ YasnecovFI8/app/build.gradle.kts
@@ -34,6 +34,10 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.squareup.okhttp3:okhttp:4.9.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.android.volley:volley:1.2.1")
+    implementation("com.squareup.picasso:picasso:2.71828")
     implementation("androidx.work:work-runtime:2.9.0")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- YasnecovFI8/app/src/main/java/com/example/yasnecovfi8/MainActivity.java
+++ YasnecovFI8/app/src/main/java/com/example/yasnecovfi8/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.yasnecovfi8;
 
+
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkManager;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.util.Log;
+
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,11 +36,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         PosledBtn = findViewById(R.id.posled_btn);
         ParalelBtn = findViewById(R.id.paralel_btn);
         DownloadBtn = findViewById(R.id.download_button);
 
-        OneTimeWorkRequest work1 =
+                OneTimeWorkRequest work1 =
                 new OneTimeWorkRequest.Builder(MyWorker.class).build();
         OneTimeWorkRequest work2 =
                 new OneTimeWorkRequest.Builder(MySecondWorker.class).build();
@@ -48,7 +67,35 @@ public class MainActivity extends AppCompatActivity {
         DownloadBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                // Создаем объект RequestQueue, для отпраки запросов
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                String url ="https://random.dog/woof.json";
+
+                // Запрашиваем изображение по URL
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                        (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    // Получаем URL изображения из ответа
+                                    String imageUrl = response.getString("url");
+                                    // Запрашиваем изображение по URL
+                                    Picasso.get().load(imageUrl).into((ImageView) findViewById(R.id.imageView));
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        });
+                // Добавляем запрос в очередь
+                queue.add(jsonObjectRequest);
+
             }
         });
     }
