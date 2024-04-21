c95b147438bc2ae5a84ba32ac00527376bca936d
Leturgone <finekon221@gmail.com>
2024-04-21 19:32:24 +0300

    УБран фрагмент

--- YasnecovFI10/app/src/main/java/com/example/yasnecovfi10/BaseActivity.java
+++ YasnecovFI10/app/src/main/java/com/example/yasnecovfi10/BaseActivity.java
@@ -1,15 +1,37 @@
 package com.example.yasnecovfi10;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class BaseActivity extends AppCompatActivity {
     MyDatabaseHelper myDB;
+    EditText editMovieTitle, editDirector, editYear, editDescription, editLength;
+    ImageView editImage;
+    Button saveButton, updateButton, deleteButton,findButton;
+
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+
+        editMovieTitle = findViewById(R.id.editMovieTitle);
+        editDirector = findViewById(R.id.editDirector);
+        editYear = findViewById(R.id.editYear);
+        editDescription = findViewById(R.id.editDescription);
+        editLength = findViewById(R.id.editLength);
+        editImage = findViewById(R.id.imgGallery);
+        saveButton = findViewById(R.id.save_button);
+        updateButton = findViewById(R.id.update_button);
+        deleteButton = findViewById(R.id.delete_button);
+        findButton = findViewById(R.id.find_button);
+
         myDB = new MyDatabaseHelper(BaseActivity.this);
+        
     }
 }
