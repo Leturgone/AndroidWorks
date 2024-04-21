29cdfec15543d2a59b516c80a57575954354275a
Leturgone <finekon221@gmail.com>
2024-04-21 14:13:18 +0300

    Добавлена активность для добавления

--- YasnecovFI10/app/src/main/java/com/example/yasnecovfi10/AddActivity.java
+++ YasnecovFI10/app/src/main/java/com/example/yasnecovfi10/AddActivity.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class BaseActivity extends AppCompatActivity {
-    MyDatabaseHelper myDB;
+public class AddActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base);
-        myDB = new MyDatabaseHelper(BaseActivity.this);
+        setContentView(R.layout.activity_add);
     }
 }
