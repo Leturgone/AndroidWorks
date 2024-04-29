2ec4c9737eec7ed77adf6a027b0b41d71980b315
Leturgone <finekon221@gmail.com>
2024-04-29 13:46:11 +0300

    Сделан WebView

--- YasnecovFI11/app/src/main/java/com/example/yasnecovfi11/MainActivity.java
+++ YasnecovFI11/app/src/main/java/com/example/yasnecovfi11/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.yasnecovfi11;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private WebView webView;
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView  = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new WebViewClient()); //Помогает приложению открывать ссылки внутри WebView, а не во внешнем браузере
+        webView.getSettings().setJavaScriptEnabled(true); //Включаем поддержку JavaScript
+
+        editText = findViewById(R.id.editTextText);
+        String url = editText.getText().toString();
+        webView.loadUrl(url); //Загрузка страницы
     }
 }
