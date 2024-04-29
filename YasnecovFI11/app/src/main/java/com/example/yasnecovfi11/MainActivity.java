a4c140d2c6316c8b223109da183c832a6f443ebe
Leturgone <finekon221@gmail.com>
2024-04-29 13:52:52 +0300

    Задание 1

--- YasnecovFI11/app/src/main/java/com/example/yasnecovfi11/MainActivity.java
+++ YasnecovFI11/app/src/main/java/com/example/yasnecovfi11/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.yasnecovfi11;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
     private EditText editText;
+    private Button enter_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +23,14 @@ public class MainActivity extends AppCompatActivity {
         webView.setWebViewClient(new WebViewClient()); //Помогает приложению открывать ссылки внутри WebView, а не во внешнем браузере
         webView.getSettings().setJavaScriptEnabled(true); //Включаем поддержку JavaScript
 
+        enter_button = findViewById(R.id.enterBtn);
         editText = findViewById(R.id.editTextText);
-        String url = editText.getText().toString();
-        webView.loadUrl(url); //Загрузка страницы
+        enter_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = editText.getText().toString();
+                webView.loadUrl(url); //Загрузка страницы
+            }
+        });
     }
 }
