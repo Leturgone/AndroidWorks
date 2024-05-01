2e2945598fbd952ba7ac6cc17829a85559302a44
Leturgone <145966860+Leturgone@users.noreply.github.com>
2024-05-02 01:42:56 +0300

    Сделано

--- YasnecovFI9/app/src/main/java/com/example/yasnecovfi9/MainActivity.java
+++ YasnecovFI9/app/src/main/java/com/example/yasnecovfi9/MainActivity.java
@@ -11,7 +11,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
